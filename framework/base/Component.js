85e83c2b2fe142a473f5dae7cc96fb0a36935b1a
e.tokovenko <e.tokovenko@gmail.com>
2016-02-18 13:58:03 +0200

    add component base class

---
+++ framework/base/Component.js
@@ -0,0 +1,24 @@
+class Component {
+    constructor() {
+        this.behaviors = [];
+    }
+
+    // attachBehavior()	Attaches a behavior to this component.
+    // attachBehaviors()	Attaches a list of behaviors to the component.
+    // behaviors()	Returns a list of behaviors that this component should behave as.
+    // canGetProperty()	Returns a value indicating whether a property can be read.
+    // canSetProperty()	Returns a value indicating whether a property can be set.
+    // className()	Returns the fully qualified name of this class.
+    // detachBehavior()	Detaches a behavior from the component.
+    // detachBehaviors()	Detaches all behaviors from the component.
+    // ensureBehaviors()	Makes sure that the behaviors declared in behaviors() are attached to this component.
+    // getBehavior()	Returns the named behavior object.
+    // getBehaviors()	Returns all behaviors attached to this component.
+    // hasEventHandlers()	Returns a value indicating whether there is any handler attached to the named event.
+    // hasMethod()	Returns a value indicating whether a method is defined.
+    // hasProperty()	Returns a value indicating whether a property is defined for this component.
+    // init()	Initializes the object.
+    // off()	Detaches an existing event handler from this component.
+    // on()	Attaches an event handler to an event.
+    // trigger()	Triggers an event.
+}
