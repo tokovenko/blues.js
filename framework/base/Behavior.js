37d8c86396a4133b4e8cdc599e4368fa770d163d
e.tokovenko <e.tokovenko@gmail.com>
2016-02-18 13:58:55 +0200

    add behavior and event base classes

---
+++ framework/base/Behavior.js
@@ -0,0 +1,12 @@
+class Behavior {
+    constructor() {
+        this.owner;
+    }
+
+    // attach()	Attaches the behavior object to the component.
+    // detach()	Detaches the behavior object from the component.
+    // events()	Declares event handlers for the $owner's events.
+}
+
+
+module.exports = Behavior;
---
+++ framework/base/Event.js
@@ -0,0 +1,12 @@
+class Event {
+    constructor() {
+        this.data = null;
+        this.name = null;
+        this.owner = null;
+    }
+
+    // hasHandlers()	Returns a value indicating whether there is any handler attached to the specified class-level event.
+    // off()	Detaches an event handler from a class-level event.
+    // on()	Attaches an event handler to a class-level event.
+    // trigger()	Triggers a class-level event.
+}
